b057d9eaa5e56b9b21eab1c0c04ac00f78977d17
Martin Hunt <mh12@sanger.ac.uk>
2016-02-15 09:17:06 +0000

    Remove dependency on mummer2crunch.py; add promer option

--- python/multi_act.py
+++ python/multi_act.py
@@ -11,11 +11,11 @@ parser = argparse.ArgumentParser(
     description = '''Compares FASTA files with blast or nucmer, writes input files for ACT.
                    Then start ACT. Files from top to bottom in ACT are same as order
                    listed on command line when this script is run.''',
-    usage = '%(prog)s [options] <blast|nucmer> <outprefix> <file1.fa> <file2.fa>  [<file3.fa ...]')
+    usage = '%(prog)s [options] <blast|nucmer|promer> <outprefix> <file1.fa> <file2.fa>  [<file3.fa ...]')
 parser.add_argument('--blast_ops', help='blastall options [%(default)s]', default='-p blastn -m 8 -F F -e 0.01 -b 10000 -v 10000')
-parser.add_argument('--nucmer_ops', help='nucmer options [%(default)s]', default='--nosimplify --maxmatch')
+parser.add_argument('--nucmer_ops', help='nucmer or promer options [promer:--maxmatch. nucmer: --maxmatch --nosimplify]')
 parser.add_argument('--delta_ops', help='delta-filter options [%(default)s]', default='-m')
-parser.add_argument('aln_tool', help='blast or nucmer')
+parser.add_argument('aln_tool', help='blast, nucmer or promer')
 parser.add_argument('outprefix', help='Prefix of output files')
 parser.add_argument('fa_list', help='List of fasta files', nargs=argparse.REMAINDER)
 options = parser.parse_args()
@@ -28,9 +28,8 @@ def index_to_union(ops, i):
 
 def compare_with_blast(qry, ref, ops, outfile):
     subprocess.check_output('formatdb -l ' + ops.outprefix + '.formatdb.log -p F -i ' + ref, shell=True)
-    subprocess.check_output('formatdb -l ' + options.outprefix + '.formatdb.log -p F -i ' + ref, shell=True)
     cmd = ' '.join([
-        'blastall', options.blast_ops,
+        'blastall', ops.blast_ops,
         '-d', ref,
         '-i', qry,
         '-o', outfile
@@ -44,8 +43,14 @@ def compare_with_nucmer(qry, ref, ops, outfile):
     filtered_file = delta_file + '.filter'
     coords_file = filtered_file + '.coords'
 
+    if ops.nucmer_ops is None:
+        if ops.aln_tool == 'promer':
+            ops.nucmer_ops = '--maxmatch'
+        else:
+            ops.nucmer_ops = '--maxmatch --nosimplify'
+
     cmd = ' '.join([
-        'nucmer',
+        ops.aln_tool,
         ops.nucmer_ops,
         '-p', nucmer_out,
         ref,
@@ -71,11 +76,9 @@ def compare_with_nucmer(qry, ref, ops, outfile):
     print('cmd:', cmd)
     pyfastaq.utils.syscall(cmd)
 
-    cmd = 'mummer2crunch.py ' + coords_file + ' ' + outfile
-    print('cmd:', cmd)
-    pyfastaq.utils.syscall(cmd)
-
-
+    pyfastaq.utils.syscall('samtools faidx ' + qry)
+    pyfastaq.utils.syscall('samtools faidx ' + ref)
+    pymummer.coords_file.convert_to_msp_crunch(coords_file, outfile, qry + '.fai', ref + '.fai')
 
 
 # make union files
@@ -101,7 +104,7 @@ for i in range(len(options.fa_list)-1):
 
     if options.aln_tool == 'blast':
         compare_with_blast(qry, ref, options, outfile)
-    elif options.aln_tool == 'nucmer':
+    elif options.aln_tool in ['nucmer', 'promer']:
         compare_with_nucmer(qry, ref, options, outfile)
     else:
         sys.exit('Unknown alignment tool:' + options.aln_tool)

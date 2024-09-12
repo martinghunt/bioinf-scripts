43018bc8b912c905e471a236b0c6a9ff994c918b
Martin Hunt <martinghunt@users.noreply.github.com>
2024-09-12 09:55:33 +0100

    Get all descendents of input project

--- python/osf_get_files_for_project.py
+++ python/osf_get_files_for_project.py
@@ -6,6 +6,55 @@ import requests
 import json
 
 
+def get_project_title(node_id):
+    url = f"https://api.osf.io/v2/nodes/{node_id}/"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        raise Exception(
+            f"Failed to fetch project title for node {node_id}: {response.status_code}"
+        )
+
+    try:
+        return data["data"]["attributes"]["title"]
+    except:
+        raise Exception(f"No title found for {node_id}\n{data}")
+
+
+def get_all_children(node_id):
+    url = f"https://api.osf.io/v2/nodes/{node_id}/children/"
+    all_nodes = {}
+
+    while url:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            current_level_nodes = {
+                child["id"]: {
+                    "title": child["attributes"]["title"],
+                    "parent_id": node_id,
+                }
+                for child in data["data"]
+            }
+            all_nodes.update(current_level_nodes)
+
+            # Recursively get children of the current children
+            for child_node_id in current_level_nodes:
+                all_nodes.update(get_all_children(child_node_id))
+
+            url = data["links"].get("next")
+        else:
+            raise Exception(
+                f"Failed to fetch components for node {node_id}: {response.status_code}"
+            )
+
+    return all_nodes
+
+
 def get_one_page_of_data(project_id, path=None, page=1):
     url = f"https://api.osf.io/v2/nodes/{project_id}/files/osfstorage"
     if path is not None:
@@ -20,7 +69,7 @@ def get_one_page_of_data(project_id, path=None, page=1):
         raise Exception(f"Error getting url: {response.url}")
 
 
-def get_all_data(project_id, path=None):
+def get_all_files_one_project(project_id, path=None):
     page = 1
     results = []
     while True:
@@ -47,7 +96,7 @@ def get_files_from_folders_iter(project_id, results):
             continue
         elif d["attributes"]["kind"] == "folder":
             path = d["attributes"]["path"].replace("/", "")
-            to_replace[i] = get_all_data(project_id, path=path)
+            to_replace[i] = get_all_files_one_project(project_id, path=path)
         else:
             raise NotImplementedError(
                 f"Unknown attribute/kind: {d['attributes']['kind']}\n{d}"
@@ -66,24 +115,59 @@ def get_files_from_folders(project_id, results):
 
 def print_tsv(results, size_in_bytes=False):
     lines_out = []
-    for d in results:
-        if size_in_bytes:
-            size = d["attributes"]["size"]
-        else:
-            size = round(float(d["attributes"]["size"]) / (1024 * 1024), 2)
-        lines_out.append(
-            (
-                d["attributes"]["materialized_path"].lstrip("/"),
-                d["links"]["download"],
-                d["attributes"]["extra"]["hashes"]["md5"],
-                size,
+    for node_id in results:
+        if len(results[node_id]["files"]) == 0:
+            continue
+
+        assert results[node_id]["title"] is not None
+        names = [results[node_id]["title"]]
+        parent = results[node_id]["parent_id"]
+        while parent is not None:
+            names.append(results[parent]["title"])
+            assert names[-1] is not None
+            parent = results[parent]["parent_id"]
+        names.reverse()
+        project_names = "/".join(names)
+
+        for d in results[node_id]["files"]:
+            if size_in_bytes:
+                size = d["attributes"]["size"]
+            else:
+                size = round(float(d["attributes"]["size"]) / (1024 * 1024), 2)
+            lines_out.append(
+                (
+                    project_names,
+                    d["attributes"]["materialized_path"].lstrip("/"),
+                    d["links"]["download"],
+                    d["attributes"]["extra"]["hashes"]["md5"],
+                    size,
+                )
             )
-        )
     lines_out.sort()
     size_name = "size(B)" if size_in_bytes else "size(MB)"
-    print("filename", "url", "md5", size_name, sep="\t")
+    print("project", "filename", "url", "md5", size_name, sep="\t")
     for line in lines_out:
         print(*line, sep="\t")
+
+
+def get_all_data(project_id):
+    logging.info(f"Start collecting data for {project_id}")
+    data = get_all_children(project_id)
+    logging.info(
+        f"Found all descendents of {options.project_id}: "
+        + ",".join(sorted(list(data.keys())))
+    )
+    assert project_id not in data
+    data[project_id] = {
+        "parent_id": None,
+        "title": get_project_title(project_id),
+    }
+
+    for proj_id in data:
+        results = get_all_files_one_project(proj_id)
+        get_files_from_folders(proj_id, results)
+        data[proj_id]["files"] = results
+    return data
 
 
 parser = argparse.ArgumentParser(
@@ -108,8 +192,6 @@ log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 results = get_all_data(options.project_id)
-logging.info("Getting folders...")
-get_files_from_folders(options.project_id, results)
 
 if options.format == "json":
     print(json.dumps(results, indent=2))
